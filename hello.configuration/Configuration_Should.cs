bfe27be8febd24318c68fb41a2703f5d238b68e1
mkoertgen <marcel.koertgen@gmail.com>
2017-03-22 16:04:05 +0100

    Initial commit.

---
+++ hello.configuration/Configuration_Should.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+
+namespace hello.configuration
+{
+    [TestFixture]
+    // ReSharper disable InconsistentNaming
+    internal class Configuration_Should
+    {
+        [Test]
+        public void Support_Command_Line_Args()
+        {
+            var args = new[]
+            {
+                "CmdArgKey1=Value1", "--CmdArgKey2=Value2", "/CmdArgKey3=Value3", "-CmdArgKey4=Value4", "--CmdArgKey5",
+                "Value5"
+            };
+            var switchMappings = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+            {
+                {"--CmdArgKey2", "cak2"},
+                {"-CmdArgKey4", "cak4"},
+            };
+            var config = new ConfigurationBuilder().AddCommandLine(args, switchMappings).Build();
+
+            config["CmdArgKey1"].Should().Be("Value1");
+            //Config["CmdArgKey2"].Should().Be("Value2");
+            config["cak2"].Should().Be("Value2");
+
+            config["CmdArgKey3"].Should().Be("Value3");
+            //Config["CmdArgKey4"].Should().Be("Value4");
+            config["cak4"].Should().Be("Value4");
+            config["CmdArgKey5"].Should().Be("Value5");
+        }
+
+        [Test]
+        public void Support_Environment_Variables()
+        {
+            Environment.SetEnvironmentVariable("env_var_key1", "env_var_value1");
+            Environment.SetEnvironmentVariable("env_var_key2", "env_var_value2");
+
+            var config = new ConfigurationBuilder().AddEnvironmentVariables().Build();
+            config["env_var_key1"].Should().Be("env_var_value1");
+            config["env_var_key2"].Should().Be("env_var_value2");
+        }
+
+        [Test]
+        public void Support_InMemory()
+        {
+            var initialConfig = new Dictionary<string, string>
+            {
+                {"mem_key1", "mem_value1"},
+                {"mem_key2", "mem_value2"}
+            };
+
+            var config = new ConfigurationBuilder().AddInMemoryCollection(initialConfig).Build();
+            config["mem_key1"].Should().Be("mem_value1");
+            config["mem_key2"].Should().Be("mem_value2");
+        }
+
+        [Test]
+        public void Support_Ini_files()
+        {
+            var config = new ConfigurationBuilder().AddIniFile("settings.ini").Build();
+            config["ini-sec:ini-key1"].Should().Be("ini-value1");
+            config["ini-sec:ini-key2"].Should().Be("ini-value2");
+
+            var section = config.GetSection("ini-sec");
+            section["ini-key1"].Should().Be("ini-value1");
+            section["ini-key2"].Should().Be("ini-value2");
+        }
+
+        [Test]
+        public void Support_Json_files()
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("settings.json").Build();
+            config["Application:Mode"].Should().Be("SampleJsonValue");
+            config["Application:Width"].Should().Be("42");
+        }
+
+        [Test]
+        public void Support_Xml_files()
+        {
+            var config = new ConfigurationBuilder().AddXmlFile("settings.xml").Build();
+            config["from"].Should().Be("Jsinh");
+            config["body:subject"].Should().Be("Hello world");
+            config["body2:subject"].Should().Be("Hello Universe");
+        }
+
+        [Test]
+        public void Override_keys_case_insensitive()
+        {
+            var first = new Dictionary<string, string> {{"key", "foo"}};
+            var second = new Dictionary<string, string> {{"KeY", "bar"}};
+            var config = new ConfigurationBuilder()
+                .AddInMemoryCollection(first)
+                .AddInMemoryCollection(second).Build();
+            config["key"].Should().Be("bar");
+        }
+    }
+}
--- hello.configuration/Program.cs
+++ hello.configuration/Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace hello.configuration
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        // cf.: https://blog.jsinh.in/asp-net-5-configuration-microsoft-framework-configurationmodel/
+        private static void Main(string[] args)
         {
+            var initialConfig = new Dictionary<string, string>
+            {
+                {"key1", "value1"},
+                {"key2", "value2"}
+            };
+
+            var config = GetConfig(args, initialConfig);
+
+            Console.WriteLine("Configuration:");
+            foreach (var kvp in config.AsEnumerable())
+                Console.WriteLine($"  {kvp.Key} = {kvp.Value}");
+        }
+
+        private static IConfiguration GetConfig(string[] args, IDictionary<string,string> initialConfig)
+        {
+            var builder = new ConfigurationBuilder();
+
+            builder
+                .AddInMemoryCollection(initialConfig)
+                .AddXmlFile("settings.xml")
+                .AddJsonFile("settings.json")
+                .AddEnvironmentVariables("prefix_")
+                .AddCommandLine(args);
+
+            var config = builder.Build();
+
+            return config;
         }
     }
 }
